0c74e15db181adea498dcd565db5d383d31a73a3
Fry-James <56590496+Fry-James@users.noreply.github.com>
2020-11-25 10:11:44 +0300

    Add files via upload

---
+++ WD.sql
@@ -0,0 +1,158 @@
+create database [WD]
+go
+use [WD]
+go
+----- Авторизация
+create table [dbo].[Authorisation]
+(
+	[ID_Authorisation] [int] not null identity(1,1),
+	[Login] [varchar] (25) not null,
+	[Password] [varchar] (25) not null,
+	constraint [PK_Authorisation] primary key clustered ([ID_Authorisation] ASC) on [PRIMARY],
+)
+go
+
+create procedure [dbo].[Authorisation_Insert]
+	@Login [varchar] (25), @Password [varchar] (25)
+as
+	 insert into [dbo].[Authorisation] ([Login], [Password]) 
+	 values (@Login, @Password)
+go
+
+create procedure [dbo].[Authorisation_Update]
+@ID_Authorisation [int], @Login [varchar] (25), @Password [varchar] (25)
+as
+	update [dbo].[Authorisation] set
+	[Login] = @Login,
+	[Password] = @Password
+	where
+		[ID_Authorisation] = @ID_Authorisation
+go
+
+create procedure [dbo].[Authorisation_Delete]
+@ID_Authorisation [int]
+as
+	delete from  [dbo].[Authorisation]
+	where
+		[ID_Authorisation] = @ID_Authorisation
+go
+
+------Должность
+Create table [dbo].[Position]
+(
+	[ID_Position] [int] not null identity(1,1),
+	[PositionName] [varchar] (25) not null,
+	[Salary] [int] not null,
+	constraint [PK_Position] primary key clustered ([ID_Position] ASC) on [PRIMARY],
+)
+go
+
+create procedure [dbo].[Position_Insert]
+	@PositionName[varchar] (25), @Salary [int]
+as
+	 insert into [dbo].[Position] ([PositionName], [Salary]) 
+	 values (@PositionName, @Salary)
+go
+
+create procedure [dbo].[Position_Update]
+@ID_Position [int], @PositionName [varchar] (25), @Salary [int]
+as
+	update [dbo].[Position] set
+	[PositionName] = @PositionName,
+	[Salary] = @Salary
+	where
+		[ID_Position] = @ID_Position
+go
+
+create procedure [dbo].[Position_Delete]
+@ID_Position [int]
+as
+	delete from  [dbo].[Position]
+	where
+		[ID_Position] = @ID_Position
+go
+
+----- Сотрудники
+
+create table [dbo].[Employee]
+(
+	[ID_Employee] [int] not null identity(1,1),
+	[FirstName] [varchar] (25) not null,
+	[MidleName] [varchar] (25) not null,
+	[LastName] [varchar] (25) not null,
+	[Position_ID] [int] not null,
+	[Admin] [bit] not null default (0),
+	constraint [PK_Authorisation] primary key clustered ([ID_Employee] ASC) on [PRIMARY],
+	constraint [FK_Position] foreign key ([Position_ID]) references [dbo].[Position] ([ID_Position])
+)
+go
+
+create procedure [dbo].[Employee_Insert]
+	@FirstName [varchar] (25), @MidleName [varchar] (25), @LastName [varchar] (25), @Position_ID [int], @Admin [bit]
+as
+	 insert into [dbo].[Employee] ([FirstName], [MidleName], [LastName], [Position_ID], [Admin]) 
+	 values (@FirstName, @MidleName, @LastName, @Position_ID, @Admin)
+go
+
+create procedure [dbo].[Employee_Update]
+@ID_Employee [int], @FirstName [varchar] (25), @MidleName [varchar] (25), @LastName [varchar] (25), @Position_ID [int], @Admin [bit]
+as
+	update [dbo].[Employee] set
+	[FirstName] = @FirstName,
+	[MidleName] = @MidleName,
+	[LastName] = @LastName,
+	[Position_ID] = @Position_ID,
+	[Admin] = @Admin
+	where
+		[ID_Employee] = @ID_Employee
+go
+
+create procedure [dbo].[Employee_Delete]
+@ID_Employee [int]
+as
+	delete from  [dbo].[Employee]
+	where
+		[ID_Employee] = @ID_Employee
+go
+
+----- Отчёт
+create table [dbo].[Report]
+(
+	[ID_Report] [int] not null identity(1,1),
+	[ReportDate] [varchar] (25) not null,
+	[ComputerName] [varchar] (25) not null,
+	[CharCount] [int] not null,
+	[LazyTime]  [varchar] (25) not null,
+	[Autorisation_ID] [int] not null,
+	constraint [PK_Report] primary key clustered ([ID_Report] ASC) on [PRIMARY],
+	constraint [FK_Autorisation] foreign key ([Autorisation_ID]) references [dbo].[Position] ([ID_Position])
+)
+go
+
+create procedure [dbo].[Report_Insert]
+	@ReportDate [varchar] (25), @ComputerName [varchar] (25), @CharCount [int], @LazyTime [varchar] (25), @Autorisation_ID [int] 
+as
+	 insert into [dbo].[Authorisation] ([Login], [Password]) 
+	 values (@ReportDate, @ComputerName, @CharCount, @LazyTime, @Autorisation_ID)
+go
+
+create procedure [dbo].[Report_Update]
+@ID_Report [int] , @ReportDate [varchar] (25), @ComputerName [varchar] (25), @CharCount [int], @LazyTime [varchar] (25), @Autorisation_ID [int]
+as
+	update [dbo].[Report] set
+	[ReportDate] = @ReportDate,
+	[ComputerName] = @ComputerName,
+	[CharCount] = @CharCount,
+	[LazyTime] = @LazyTime,
+	[Autorisation_ID] = @Autorisation_ID
+	where
+		[ID_Report] = @ID_Report
+go
+
+create procedure [dbo].[Report_Delete]
+@ID_Report [int]
+as
+	delete from  [dbo].[Report]
+	where
+		[ID_Report] = @ID_Report
+go
